94667d4513c1b06ec7a8df5ad75a830cb67e8129
Lukáš Friedl <lukys3036@gmail.com>
2025-01-09 08:46:33 +0100

    Add isolation security config variable

--- src/main/java/com/trup10ka/kappa/config/KappaConfig.java
+++ src/main/java/com/trup10ka/kappa/config/KappaConfig.java
@@ -1,11 +1,14 @@
 package com.trup10ka.kappa.config;
 
+import com.trup10ka.kappa.util.IsolationSecurity;
 import org.jetbrains.annotations.NotNull;
 
 public record KappaConfig(
         @NotNull String dbName,
         @NotNull String dbUser,
         @NotNull String dbPassword,
-        @NotNull String dbHost
-) {
+        @NotNull String dbHost,
+        @NotNull IsolationSecurity dbIsolationSecurity
+)
+{
 }
