395a90e893f95e3ac96b7513aaa6eb560bd95fa8
Trup10ka <lukys3036@gmail.com>
2025-01-13 00:06:33 +0100

    Wrap cli client and db client in KappaApp

---
+++ src/main/java/com/trup10ka/kappa/KappaApp.java
@@ -0,0 +1,48 @@
+package com.trup10ka.kappa;
+
+import com.trup10ka.kappa.cli.CliClient;
+import com.trup10ka.kappa.config.ConfigLoader;
+import com.trup10ka.kappa.config.FileConfigLoader;
+import com.trup10ka.kappa.config.KappaConfig;
+import com.trup10ka.kappa.db.DbClient;
+import com.trup10ka.kappa.db.HikariDbClient;
+import com.trup10ka.kappa.exceptions.InvalidConfigException;
+import org.jetbrains.annotations.NotNull;
+
+public class KappaApp
+{
+    @NotNull
+    private final ConfigLoader configLoader = new FileConfigLoader("config.conf");
+
+    private CliClient cliClient;
+
+    public void init()
+    {
+        KappaConfig config = configLoader.loadConfig();
+
+        DbClient hikariDbClient = new HikariDbClient(config);
+        hikariDbClient.init();
+        testDbClientConnection(hikariDbClient);
+
+        cliClient = new CliClient(hikariDbClient);
+    }
+
+    public void run()
+    {
+        cliClient.start();
+    }
+
+    private void testDbClientConnection(DbClient dbClient)
+    {
+        try
+        {
+            dbClient.testConnection();
+        }
+        catch (InvalidConfigException e)
+        {
+            System.out.println("Failed to connect to the database. Exiting...");
+            dbClient.close();
+            System.exit(1);
+        }
+    }
+}
--- src/main/java/com/trup10ka/kappa/Main.java
+++ src/main/java/com/trup10ka/kappa/Main.java
@@ -1,14 +1,11 @@
 package com.trup10ka.kappa;
 
-import com.trup10ka.kappa.cli.CliClient;
-
 public class Main
 {
     public static void main(String[] args)
     {
-        CliClient cliClient = new CliClient();
-
-        cliClient.init();
-        cliClient.start();
+        KappaApp app = new KappaApp();
+        app.init();
+        app.run();
     }
 }
