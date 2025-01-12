3d6f14f09fa842a386261cb9e1f965bd1191ef2e
Trup10ka <lukys3036@gmail.com>
2025-01-12 18:21:20 +0100

    Add exception handling in FileConfigLoader

--- src/main/java/com/trup10ka/kappa/config/FileConfigLoader.java
+++ src/main/java/com/trup10ka/kappa/config/FileConfigLoader.java
@@ -3,12 +3,16 @@ package com.trup10ka.kappa.config;
 
 import com.electronwill.nightconfig.core.file.FileConfig;
 import com.electronwill.nightconfig.core.file.NoFormatFoundException;
+import com.trup10ka.kappa.util.IsolationSecurity;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
 
 import static com.trup10ka.kappa.util.FileUtil.copyFileIfNotExists;
 
 public class FileConfigLoader implements ConfigLoader
 {
+    private final String[] requiredFields = { "db-name", "db-user", "db-password", "db-host", "db-isolation-security" };
 
     private final String filePath;
 
@@ -25,12 +29,16 @@ public class FileConfigLoader implements ConfigLoader
         try (FileConfig fileConfig = FileConfig.of(filePath))
         {
             fileConfig.load();
+            checkIfAnyFiledInConfigIsMissing(fileConfig);
+
+            IsolationSecurity isolationSecurity = parseIsolationSecurity(fileConfig);
 
             return new KappaConfig(
                     fileConfig.get("db-name"),
                     fileConfig.get("db-user"),
                     fileConfig.get("db-password"),
-                    fileConfig.get("db-host")
+                    fileConfig.get("db-host"),
+                    isolationSecurity
             );
         }
         catch (NoFormatFoundException e)
@@ -38,6 +46,33 @@ public class FileConfigLoader implements ConfigLoader
             copyFileIfNotExists("config.conf", "config.conf");
             System.exit(1);
             return null;
+        }
+    }
+
+    private IsolationSecurity parseIsolationSecurity(FileConfig fileConfig)
+    {
+        String isolationSecurityAsString = fileConfig.get("db-isolation-security");
+        try
+        {
+            return IsolationSecurity.valueOf(isolationSecurityAsString);
+        }
+        catch (IllegalArgumentException ignored)
+        {
+            System.out.println("Invalid isolation security value: " + isolationSecurityAsString + ". Available candidates are: " + Arrays.toString(IsolationSecurity.values()));
+            System.exit(1);
+        }
+        return null;
+    }
+
+    private void checkIfAnyFiledInConfigIsMissing(FileConfig fileConfig)
+    {
+        for (String requiredField : requiredFields)
+        {
+            if (!fileConfig.contains(requiredField))
+            {
+                System.out.println("Missing required field in the config file: " + requiredField);
+                System.exit(1);
+            }
         }
     }
 }
