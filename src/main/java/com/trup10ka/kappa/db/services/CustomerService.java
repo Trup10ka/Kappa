e913633eb6bde55ac17d516e45aeca1276deff13
Lukáš Friedl <lukys3036@gmail.com>
2025-01-13 18:21:43 +0100

    Implement HikariCustomerService

--- src/main/java/com/trup10ka/kappa/db/services/CustomerService.java
+++ src/main/java/com/trup10ka/kappa/db/services/CustomerService.java
@@ -1,11 +1,28 @@
 package com.trup10ka.kappa.db.services;
 
 import com.trup10ka.kappa.data.Customer;
+import com.trup10ka.kappa.data.CustomerSex;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public interface CustomerService
 {
-    int addCustomer();
-    boolean deleteCustomer();
-    boolean updateCustomer();
-    Customer getCustomer();
+    int addCustomer(
+            @NotNull String firstName,
+            @NotNull String lastName,
+            @NotNull CustomerSex email,
+            int customerCredits
+    );
+
+    boolean deleteCustomerByName(@NotNull String firstName, @NotNull String lastName);
+
+    boolean deleteCustomerById(int customerId);
+
+    boolean updateCustomer(int customerId, Customer customer);
+
+    @Nullable
+    Customer getCustomerById(int customerId);
+
+    @Nullable
+    Customer getCustomerByName(@NotNull String firstName, @NotNull String lastName);
 }
--- src/main/java/com/trup10ka/kappa/db/services/HikariCustomerService.java
+++ src/main/java/com/trup10ka/kappa/db/services/HikariCustomerService.java
@@ -1,43 +1,203 @@
 package com.trup10ka.kappa.db.services;
 
 import com.trup10ka.kappa.data.Customer;
+import com.trup10ka.kappa.data.CustomerSex;
 import com.trup10ka.kappa.db.DbClient;
-import com.trup10ka.kappa.db.KappaTransactionManager;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.sql.*;
 
 public class HikariCustomerService implements CustomerService
 {
 
     private final DbClient dbClient;
 
-    private final KappaTransactionManager transactionManager;
-
-    public HikariCustomerService(DbClient dbClient, KappaTransactionManager transactionManager)
+    public HikariCustomerService(DbClient dbClient)
     {
         this.dbClient = dbClient;
-        this.transactionManager = transactionManager;
     }
 
     @Override
-    public int addCustomer()
+    public int addCustomer(@NotNull String firstName, @NotNull String lastName, @NotNull CustomerSex sex, int customerCredits)
     {
-        return 0;
+        Customer customer = new Customer(firstName, lastName, sex, customerCredits);
+
+        String insertCustomer = "INSERT INTO customer (first_name, last_name, sex, customer_credits) VALUES (?, ?, ?, ?)";
+
+        try (var connection = dbClient.getDataSource().getConnection())
+        {
+            PreparedStatement statement = createStatementToInsertCustomer(connection, customer, insertCustomer);
+            int affectedRows = statement.executeUpdate();
+
+            if (affectedRows == 0) return -1;
+
+            return returnCustomerId(statement);
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+        return -1;
     }
 
     @Override
-    public boolean deleteCustomer()
+    public boolean deleteCustomerByName(@NotNull String firstName, @NotNull String lastName)
     {
+        String deleteCustomer = "DELETE FROM customer WHERE first_name = ? AND last_name = ?";
+
+        try (var connection = dbClient.getDataSource().getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(deleteCustomer);
+            int affectedRows = statement.executeUpdate();
+
+            return affectedRows > 0;
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
         return false;
     }
 
     @Override
-    public boolean updateCustomer()
+    public boolean deleteCustomerById(int customerId)
     {
+        String deleteCustomer = "DELETE FROM customer WHERE customer_id = ?";
+
+        try (var connection = dbClient.getDataSource().getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(deleteCustomer);
+            statement.setInt(1, customerId);
+            int affectedRows = statement.executeUpdate();
+
+            return affectedRows > 0;
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
         return false;
     }
 
     @Override
-    public Customer getCustomer()
+    public boolean updateCustomer(int customerId, Customer customer)
     {
+        String update = "UPDATE customer SET first_name = ?, last_name = ?, sex = ?, customer_credits = ? WHERE customer_id = ?";
+
+        try (var connection = dbClient.getDataSource().getConnection())
+        {
+            PreparedStatement statement = createUpdateStatement(connection, update, customer, customerId);
+            int affectedRows = statement.executeUpdate();
+
+            return affectedRows > 0;
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        return false;
+    }
+
+    @Override
+    @Nullable
+    public Customer getCustomerById(int customerId)
+    {
+        String selectCustomer = "SELECT * FROM customer WHERE customer_id = ?";
+
+        try (var connection = dbClient.getDataSource().getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(selectCustomer);
+            statement.setInt(1, customerId);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next())
+            {
+                return new Customer(
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        CustomerSex.valueOf(resultSet.getString("sex")),
+                        resultSet.getInt("customer_credits")
+                );
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
         return null;
+    }
+
+    @Override
+    public Customer getCustomerByName(@NotNull String firstName, @NotNull String lastName)
+    {
+        String selectCustomer = "SELECT * FROM customer WHERE first_name = ? AND last_name = ?";
+
+        try (var connection = dbClient.getDataSource().getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(selectCustomer);
+            statement.setString(1, firstName);
+            statement.setString(2, lastName);
+
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next())
+            {
+                return new Customer(
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name"),
+                        CustomerSex.valueOf(resultSet.getString("sex")),
+                        resultSet.getInt("customer_credits"));
+            }
+        }
+        catch (SQLException e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+        return null;
+    }
+
+    private int returnCustomerId(PreparedStatement statement) throws SQLException
+    {
+        try (ResultSet generatedKeys = statement.getGeneratedKeys())
+        {
+            if (generatedKeys.next())
+            {
+                return generatedKeys.getInt(1);
+            }
+            else
+            {
+                return -1;
+            }
+        }
+    }
+
+    private PreparedStatement createUpdateStatement(Connection connection, String sql, Customer customer, int customerId) throws SQLException
+    {
+        PreparedStatement statement = connection.prepareStatement(sql);
+        statement.setString(1, customer.firstName());
+        statement.setString(2, customer.lastName());
+        statement.setString(3, customer.sex().toString());
+        statement.setInt(4, customer.customerCredits());
+        statement.setInt(5, customerId);
+        return statement;
+    }
+
+    private PreparedStatement createStatementToInsertCustomer(Connection connection, Customer customer, String sql) throws SQLException
+    {
+        PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+        statement.setString(1, customer.firstName());
+        statement.setString(2, customer.lastName());
+        statement.setString(3, customer.sex().toString());
+        statement.setInt(4, customer.customerCredits());
+
+        return statement;
     }
 }
