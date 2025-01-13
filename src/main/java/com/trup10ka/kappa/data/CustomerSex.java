657f79d62c24db1eff70a6b984510692e2a9c670
Lukáš Friedl <lukys3036@gmail.com>
2025-01-13 18:20:28 +0100

    Add customer sex enum

---
+++ src/main/java/com/trup10ka/kappa/data/CustomerSex.java
@@ -0,0 +1,15 @@
+package com.trup10ka.kappa.data;
+
+public enum CustomerSex
+{
+    MALE("MALE"),
+    FEMALE("MALE"),
+    OTHER("MALE");
+
+    public final String value;
+
+    CustomerSex(String value)
+    {
+        this.value = value;
+    }
+}
