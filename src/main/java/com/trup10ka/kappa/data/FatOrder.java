b6fb661cb063cdce35470ef423c676ba9bce2568
Trup10ka <lukys3036@gmail.com>
2025-01-17 20:36:58 +0100

    Add FatOrder object to conviently display customer order with products

---
+++ src/main/java/com/trup10ka/kappa/data/FatOrder.java
@@ -0,0 +1,25 @@
+package com.trup10ka.kappa.data;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+
+public record FatOrder(
+        @NotNull String customerFirstName,
+        @NotNull String customerLastName,
+        @NotNull Map<Product, Integer> products
+)
+{
+    @Override
+    public String toString()
+    {
+        return """
+        
+        Customer: %s %s
+        Products: %s
+        
+        """.formatted(customerFirstName, customerLastName, products.entrySet().stream()
+                .map(entry -> entry.getKey().productName() + ": " + entry.getValue())
+                .toList());
+    }
+}
